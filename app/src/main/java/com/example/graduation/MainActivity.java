64fb5b9bb32d692b48fb7bfe71c49cdc111d486e
gkdy5295 <gkdy5275@gmail.com>
2021-08-23 13:35:41 +0900

    하단네비게이션보임

--- app/src/main/java/com/example/graduation/MainActivity.java
+++ app/src/main/java/com/example/graduation/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.graduation;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.bottomNavi);
+        //AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+        //        R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+        //        .build();
+        //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+       // NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        //NavigationUI.setupWithNavController(navView, navController);
     }
+
 }
