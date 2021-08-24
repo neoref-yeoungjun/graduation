ba283487e0bd4f3dc7d52202fe344ff13f57301d
gkdy5295 <gkdy5275@gmail.com>
2021-08-24 14:13:13 +0900

    홈, 관심강좌, 설정 프래그먼트 만들기

--- app/src/main/java/com/example/graduation/MainActivity.java
+++ app/src/main/java/com/example/graduation/MainActivity.java
@@ -1,28 +1,84 @@
 package com.example.graduation;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Main_Activity";
+
+    private BottomNavigationView bottomNavigationView;
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+    private HomeFragment home1;
+    private InterestFragment interest1;
+    private SettingFragment setting1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.bottomNavi);
-        //AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-        //        R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-        //        .build();
-        //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-       // NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        //NavigationUI.setupWithNavController(navView, navController);
+
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+
+        //첫 화면 띄우기
+       // getSupportFragmentManager().beginTransaction().add(R.id.frame_container,new home1()).commit();
+
+        //case 함수를 통해 클릭 받을 때마다 화면 변경하기
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.item_fragment1 :
+                        setFrag(0);
+                        break;
+                    case R.id.item_fragment4:
+                        setFrag(1);
+                        break;
+                    case R.id.item_fragment5:
+                        setFrag(2);
+                        break;
+
+
+                }
+                return true;
+            }
+        });
+
+        home1= new HomeFragment();
+        interest1= new InterestFragment();
+        setting1 = new SettingFragment();
+        setFrag(0);
     }
 
+    private void setFrag(int n)
+    {
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+        switch(n)
+        {
+            case 0:
+            ft.replace(R.id.frame_container,home1);
+            ft.commit();
+            break;
+
+            case 1:
+                ft.replace(R.id.frame_container,interest1);
+                ft.commit();
+                break;
+            case 2:
+                ft.replace(R.id.frame_container,setting1);
+                ft.commit();
+                break;
+
+        }
+    }
 }
