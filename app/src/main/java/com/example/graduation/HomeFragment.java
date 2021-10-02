c70e5a6ad6ecf0f204f9c1ab447d8989437c5595
jiwon <rizjw26@gmai.com>
2021-10-02 23:34:06 +0900

    마이페이지마이페이지

--- app/src/main/java/com/example/graduation/HomeFragment.java
+++ app/src/main/java/com/example/graduation/HomeFragment.java
@@ -5,8 +5,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CheckBox;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,7 +15,6 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.Collections;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -36,7 +33,6 @@ public class HomeFragment extends Fragment {
     private DatabaseReference databaseReference;
     private eduAdapter.OnItemClickListener listener;
     private View view;
-    private CheckBox checkBoxweek;
 
 
 
@@ -50,8 +46,6 @@ public class HomeFragment extends Fragment {
         layoutManager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(layoutManager);
         arrayList = new ArrayList<>(); // User 객체를 담을 어레이 리스트 (어댑터쪽으로)
-        Spinner spinner =view.findViewById(R.id.spinner);
-        String serchOption="name";
 
 
         eduAdapter.OnItemClickListener listener= new eduAdapter.OnItemClickListener() {
@@ -87,16 +81,15 @@ public class HomeFragment extends Fragment {
         };
         database = FirebaseDatabase.getInstance(); // 파이어베이스 데이터베이스 연동
         databaseReference = database.getReference("edu"); // DB 테이블 연결
-        databaseReference.orderByChild("apply_end").addListenerForSingleValueEvent(new ValueEventListener() {
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onDataChange( DataSnapshot dataSnapshot) {
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 // 파이어베이스 데이터베이스의 데이터를 받아오는 곳
                 arrayList.clear(); // 기존 배열리스트가 존재하지않게 초기화
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) { // 반복문으로 데이터 List를 추출해냄
                     edu edu = snapshot.getValue(edu.class); // 만들어뒀던 User 객체에 데이터를 담는다.
                     arrayList.add(edu); // 담은 데이터들을 배열리스트에 넣고 리사이클러뷰로 보낼 준비
                 }
-                Collections.reverse(arrayList);
                 adapter.notifyDataSetChanged(); // 리스트 저장 및 새로고침해야 반영이 됨
             }
 
