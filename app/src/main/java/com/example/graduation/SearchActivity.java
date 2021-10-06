f5cb54295159d645e52a099b30c3804e52146e09
gkdy5295 <gkdy5275@gmail.com>
2021-10-06 14:47:34 +0900

    강좌 검색 구현

--- app/src/main/java/com/example/graduation/SearchActivity.java
+++ app/src/main/java/com/example/graduation/SearchActivity.java
@@ -1,20 +1,21 @@
 package com.example.graduation;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -30,7 +31,7 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class SearchActivity extends AppCompatActivity{
+public class SearchActivity extends AppCompatActivity implements TextWatcher {
     private FragmentManager fm;
     private FragmentTransaction ft;
     private BottomNavigationView bottomNavigationView;
@@ -41,129 +42,109 @@ public class SearchActivity extends AppCompatActivity{
     EditText editText;
     private FirebaseDatabase database;
     private DatabaseReference databaseReference;
-    private ArrayList<edu> arrayList,filteredList;
-    private eduAdapter adapter;
-    private RecyclerView recyclerView;
-    private RecyclerView.LayoutManager layoutManager;
-    private eduAdapter.OnItemClickListener listener;
-    String searchOption="name";
-    String text;
-    private Button btn;
-
+    private ArrayList<edu> arrayList;
+//    private eduAdapter adapter;
+    String searchOption;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        btn = findViewById(R.id.searchBtn);
+ //       setUpRecyclerView();
+
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.item_fragment1:
+                        Intent intent = new Intent(SearchActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        break;
+
+                    case R.id.item_fragment2://이전 버튼
+                        onBackPressed();
+                        break;
+
+                    case R.id.item_fragment3://새로 고침
+
+
+                        break;
+
+                    case R.id.item_fragment4:
+                        setFrag(1);
+                        break;
+
+                    case R.id.item_fragment5:
+                        setFrag(2);
+                        break;
+
+
+                }
+                return true;
+            }
+        });
+        home1 = new HomeFragment();
+        interest1 = new InterestFragment();
+        setting1 = new SettingFragment();
+
         spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.option, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+//        spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
 
 
 
-        btn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                        @Override
-                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                            switch(position){
-                                case 0:
-                                    searchOption="name";
-                                    break;
-                                case 1:
-                                    searchOption="institution";
-                                    break;
-                                case 2:
-                                    searchOption="week";
-                                    break;
-                                case 3:
-                                    searchOption="fee";
-                                    break;
-                            }
-                        }
 
-                        @Override
-                        public void onNothingSelected(AdapterView<?> parent) {
-
-                        }
-                    });
-                    text = editText.getText().toString();
-                    databaseReference.orderByChild(searchOption).startAt(text).endAt(text).addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(DataSnapshot dataSnapshot) {
-                            // 파이어베이스 데이터베이스의 데이터를 받아오는 곳
-                            arrayList.clear(); // 기존 배열리스트가 존재하지않게 초기화
-                            for (DataSnapshot snapshot : dataSnapshot.getChildren()) { // 반복문으로 데이터 List를 추출해냄
-                                edu edu = snapshot.getValue(edu.class); // 만들어뒀던 User 객체에 데이터를 담는다.
-                                arrayList.add(edu); // 담은 데이터들을 배열리스트에 넣고 리사이클러뷰로 보낼 준비
-                            }
-                            Collections.reverse(arrayList);// 리스트 저장 및 새로고침해야 반영이 됨
-                            adapter.notifyDataSetChanged();
-                        }
-
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError databaseError) {
-                            // 디비를 가져오던중 에러 발생 시
-                            Log.e("HomeFragment", String.valueOf(databaseError.toException())); // 에러문 출력
-                        }
-                    });
-
-                }
-            });
-            recyclerView = findViewById(R.id.recycler_view_search);
-            recyclerView.setHasFixedSize(true);
-            recyclerView.setLayoutManager(layoutManager);
-            editText = findViewById(R.id.searchWord);
-            arrayList = new ArrayList<>();
-            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
-            recyclerView.setLayoutManager(linearLayoutManager);
-            database = FirebaseDatabase.getInstance(); // 파이어베이스 데이터베이스 연동
-            databaseReference = database.getReference("edu"); // DB 테이블 연결=
-
-            adapter = new eduAdapter(arrayList, getApplicationContext(), listener);
-            recyclerView.setAdapter(adapter);
-
-            bottomNavigationView = findViewById(R.id.bottomNavi);
-
-            bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    switch (item.getItemId()) {
-                        case R.id.item_fragment1:
-                            Intent intent = new Intent(SearchActivity.this, MainActivity.class);
-                            startActivity(intent);
-                            break;
-
-                        case R.id.item_fragment2://이전 버튼
-                            onBackPressed();
-                            break;
-
-                        case R.id.item_fragment3://새로 고침
-
-
-                            break;
-
-                        case R.id.item_fragment4:
-                            setFrag(1);
-                            break;
-
-                        case R.id.item_fragment5:
-                            setFrag(2);
-                            break;
-
-
-                    }
-                    return true;
-                }
-            });
-            home1 = new HomeFragment();
-            interest1 = new InterestFragment();
-            setting1 = new SettingFragment();
-
-
-        }
-
-
+    }
+//    private void setUpRecyclerView(){
+//        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view_search);
+//        recyclerView.setHasFixedSize(true); // 리사이클러뷰 기존성능 강화
+//        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+//        editText = (EditText)findViewById(R.id.searchWord);
+//        editText.addTextChangedListener(this);
+//
+//        arrayList = new ArrayList<>(); // User 객체를 담을 어레이 리스트 (어댑터쪽으로)
+//
+//        database = FirebaseDatabase.getInstance(); // 파이어베이스 데이터베이스 연동
+//        databaseReference = database.getReference("edu"); // DB 테이블 연결
+//        databaseReference.orderByChild("apply_end").addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange( DataSnapshot dataSnapshot) {
+//                // 파이어베이스 데이터베이스의 데이터를 받아오는 곳
+//                arrayList.clear(); // 기존 배열리스트가 존재하지않게 초기화
+//                for (DataSnapshot snapshot : dataSnapshot.getChildren()) { // 반복문으로 데이터 List를 추출해냄
+//                    edu edu = snapshot.getValue(edu.class); // 만들어뒀던 User 객체에 데이터를 담는다.
+//                    arrayList.add(edu); // 담은 데이터들을 배열리스트에 넣고 리사이클러뷰로 보낼 준비
+//                }
+//                Collections.reverse(arrayList);
+//                adapter.notifyDataSetChanged(); // 리스트 저장 및 새로고침해야 반영이 됨
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//                // 디비를 가져오던중 에러 발생 시
+//                Log.e("HomeFragment", String.valueOf(databaseError.toException())); // 에러문 출력
+//            }
+//        });
+//
+//        eduAdapter.OnItemClickListener listener= new eduAdapter.OnItemClickListener() {
+//
+//            @Override
+//            public void onItemClick(View v, int pos) {
+//
+//            }
+//        };
+//
+//
+//      //  adapter = new eduAdapter(arrayList ,getContext(), listener);
+//        recyclerView.setAdapter(adapter); // 리사이클러뷰에 어댑터 연결
+//    }
 
     private void setFrag(int n) {
         fm = getSupportFragmentManager();
@@ -194,12 +175,19 @@ public class SearchActivity extends AppCompatActivity{
         super.onBackPressed();
     }
 
+
     @Override
-    protected void onResume() {
-        super.onResume();
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
     }
 
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        //adapter.getFilter().filter(s);
+    }
 
+    @Override
+    public void afterTextChanged(Editable s) {
 
-
+    }
 }
