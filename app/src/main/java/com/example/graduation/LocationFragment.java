3ee82d9099316fe2532a3eec75d601301d7fa115
jiwon <rizjw26@gmai.com>
2021-10-21 04:59:04 +0900

    question 레이아웃 하단 고정 스피너 array 추가

--- app/src/main/java/com/example/graduation/LocationFragment.java
+++ app/src/main/java/com/example/graduation/LocationFragment.java
@@ -3,17 +3,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class LocationFragment extends Fragment {
+    private Spinner spinner;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.frag_location,container,false);
+        spinner = v.findViewById(R.id.spinner_loca);
+
         return v;
 
+
+
+
+
+
     }
+
 }
