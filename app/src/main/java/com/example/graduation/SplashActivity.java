27206562c0d22a357f517822a3384656771e145f
jiwon <rizjw26@gmai.com>
2021-10-20 23:57:18 +0900

    splash 자바 코드

---
+++ app/src/main/java/com/example/graduation/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.graduation;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final int SPLASH_DP_TIME = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceStare) {
+
+        super.onCreate(savedInstanceStare);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DP_TIME);
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        finish();
+    }
+}
