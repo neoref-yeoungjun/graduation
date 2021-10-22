f9a424f75e5941ae336ec23e9fd59d4034ebc1de
jiwon <rizjw26@gmai.com>
2021-10-23 01:00:28 +0900

    비밀번호  찾기 레이아웃 구성 및 인텐트 연결

---
+++ app/src/main/java/com/example/graduation/PwdActivity.java
@@ -0,0 +1,16 @@
+package com.example.graduation;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PwdActivity extends AppCompatActivity {
+    private static final String TAG = "PwdActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pwd);
+    }
+}
+
