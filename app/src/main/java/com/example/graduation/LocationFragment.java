52275e1dd45b8917be072445df220e62cafc1b16
gkdy5295 <gkdy5275@gmail.com>
2021-10-23 23:05:51 +0900

    지역별 수정중

--- app/src/main/java/com/example/graduation/LocationFragment.java
+++ app/src/main/java/com/example/graduation/LocationFragment.java
@@ -3,22 +3,115 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class LocationFragment extends Fragment {
-    private Spinner spinner;
-
+    private Spinner spinner1, spinner2;
+    ArrayAdapter<CharSequence> adspin1, adspin2;
+    String choice_do="";
+    String choice_se="";
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.frag_location,container,false);
-        spinner = v.findViewById(R.id.spinner_loca);
+        spinner1 = v.findViewById(R.id.spinner_loca);
+        spinner2 =v.findViewById(R.id.spinner_orgn);
 
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch(position){
+                    case 0:
+                        choice_do="충주";
+                        adspin2 = ArrayAdapter.createFromResource(getContext(), R.array.chungju, android.R.layout.simple_spinner_dropdown_item);
+                        adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        spinner2.setAdapter(adspin2);
+                        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                            @Override
+                            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                                switch(position){
+                                    case 0:
+                                        choice_se="전체";
+                                        Toast.makeText(view.getContext(), choice_se, Toast.LENGTH_SHORT).show();
+                                        break;
+                                    case 1:
+                                        break;
+                                    case 2:
+                                        break;
+                                    case 3:
+                                        break;
+                                    case 4:
+                                        break;
+                                    case 5:
+                                        break;
+                                    case 6:
+                                        break;
+                                    case 7:
+                                        break;
+                                    case 8:
+                                        break;
+                                    case 9:
+                                        break;
+                                    case 10:
+                                        break;
+                                    case 11:
+                                        break;
+                                    case 12:
+                                        break;
+                                    case 13:
+                                        break;
+                                    case 14:
+                                        break;
+                                    case 15:
+                                        break;
+                                }
+                            }
+
+                            @Override
+                            public void onNothingSelected(AdapterView<?> parent) {
+
+                            }
+                        });
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                    case 9:
+                        break;
+                    case 10:
+                        break;
+                    case 11:
+                        break;
+
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
         return v;
 
 
